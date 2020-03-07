83a85d23997947d5807d214e90c0c058e09b737f
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2020-03-07 11:02:49 -0300

    iniciando codigos do exercicio 1

--- INF-0611/Trabalho_1/inf0611_trabalho1.R
+++ INF-0611/Trabalho_1/inf0611_trabalho1.R
@@ -14,7 +14,7 @@
 #------------------------------------------------#
 # setwd("") # configure o caminho antes de descomentar essa linha
 source("./ranking_metrics.R")
-source("./trabalho1_base.R")
+source("../INF-0611/Trabalho_1/trabalho1_base.R")
 
 
 #------------------------------------------------#
@@ -22,18 +22,36 @@ source("./trabalho1_base.R")
 # dos dados, (o segundo parâmetro nao deve ser   #
 # modificado)                                    #
 #------------------------------------------------#
-data    <- process_data("time.txt", "XX-Text [[:alnum:]]",  "Article ", convertcase = FALSE, remove_stopwords = FALSE)
-queries <- process_data("queries.txt", "XX-Find [[:alnum:]]", "Query ", convertcase = FALSE, remove_stopwords = FALSE)
+data    <- process_data("../INF-0611/Trabalho_1/time.txt", "XX-Text [[:alnum:]]",  "Article ", convertcase = FALSE, remove_stopwords = FALSE)
+queries <- process_data("../INF-0611/Trabalho_1/queries.txt", "XX-Find [[:alnum:]]", "Query ", convertcase = FALSE, remove_stopwords = FALSE)
 
-a <- read.csv("relevance.csv", header = TRUE)
+a <- read.csv("../INF-0611/Trabalho_1/relevance.csv", header = TRUE)
 names(a)[a[1,]==1] # artigos relevantes da primeira consulta
+
+# Criando a matriz de termo-documento com frequencias
+time_words <- document_term_frequencies(time_words, term = "word"); time_words
 
 #------------------------------------------------#
 # Questao 1                                      #
 #------------------------------------------------#
+
+
+
 # Conjunto 1 de valores k e b
+k1 <- 1.2
+b1 <- 0.75
+# Computando as estatisticas: tf, idf, tf-dif, tf_bm25 e bm25
+time_words_stats1 <- as.data.frame(document_term_frequencies_statistics(time_words, k1, b1)) 
+# Visualizando as estatisticas
+head(time_words_stats1)
 
 # Conjunto 2 de valores k e b
+k2 <- 1.4
+b2 <- 0.80
+# Computando as estatisticas: tf, idf, tf-dif, tf_bm25 e bm25
+time_words_stats2 <- as.data.frame(document_term_frequencies_statistics(time_words, k2, b2)) 
+# Visualizando as estatisticas
+time_words_stats2
 
 
 #------------------------------------------------#
